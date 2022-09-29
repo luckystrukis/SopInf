c47fdfcedde4fb31aa94bb7c503eb85f8a4ebd61
Medina Lucas <80714068+luckystrukis@users.noreply.github.com>
2022-09-29 08:59:15 -0300

    Add files via upload

---
+++ taller_de_sop_info/python 15_9/fotos.py
@@ -0,0 +1,16 @@
+import cv2
+
+clasificador_de_rostros = cv2.CascadeClassifier(cv2.data.haarcascades+'haarcascade_frontalface_default.xml')
+#foto_prueba_1.jpg
+#foto_rostros_0.jpg
+image = cv2.imread('fotos/foto_rostros_0.jpg')
+copia_imagen = image.copy()
+rostro = clasificador_de_rostros.detectMultiScale(image)
+if not rostro.all():
+    print("no hay cara")
+else:
+    print(rostro)
+for (x,y,anch,alto) in rostro:
+cv2.rectangle(image,(x,y),(rostro[0]+rostro[2],rostro[1]+rostro[3],0,255,255),2)
+rostro = copia_imagen[y:rostro[1]+rostro[3],x:rostro[0]+rostro[2]]
+cv2.imwrite(rostro.jpg, rostro)
