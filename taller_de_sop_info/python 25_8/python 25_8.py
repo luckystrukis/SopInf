8173e02b7e1651085aada3b66ee120d78e863831
luckystrukis <luce2024@hotmail.com>
2022-10-24 11:06:27 -0300

    Update python 25_8.py

--- taller_de_sop_info/python 25_8/python 25_8.py
+++ taller_de_sop_info/python 25_8/python 25_8.py
@@ -8,8 +8,7 @@ pagina = requests.get("https://datos.gob.ar/dataset/cultura-mapa-cultural-agente
 
 objeto_sopa = BeautifulSoup(pagina.content, 'html.parser')
 
-e1 = objeto_sopa.find_all("a", {"class": "btn-green"})
-
+e1 = objeto_sopa.find("a", {"class": "btn-green"})
 print(e1)
 """
 csv = requests.get("https://datos.cultura.gob.ar/dataset/0560ef96-55ca-4026-b70a-d638e1541c05/resource/0d1b731b-28c9-4402-a7eb-018c9cf8958b/download/diario_impreso.csv").text
